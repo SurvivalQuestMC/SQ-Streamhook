cfb590e36ed4c9e2e1a5272330f4dc5b46b38ef6
RestedWicked <jesusdperez02@gmail.com>
2024-06-19 04:59:51 -0700

    [FEATURE] Authentication and Database are ready

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use reqwest::{header, Error};
 use serde::Deserialize;
-use sqlx::{sqlite::SqlitePool, Executor};
+use sqlx::sqlite::SqlitePool;
 use std::env;
 use tokio::task::spawn_blocking;
 
@@ -27,49 +27,100 @@ async fn main() -> Result<(), anyhow::Error> {
     dotenvy::from_filename(".env").ok();
 
     let pool = SqlitePool::connect(&env::var(DATABASE).unwrap()).await?;
-    let mut conn = pool.acquire().await?;
+    sqlx::migrate!().run(&pool).await?;
 
-    conn.execute("CREATE TABLE IF NOT EXISTS streamhooks_auth ( access_token TEXT )")
-        .await?;
-
-    validate_auth_token();
-
-    let request = spawn_blocking(move || authenticate_streamhooks().unwrap())
-        .await
-        .unwrap();
-
-    println!("Inserting into database!");
-    let id = sqlx::query!(
-        r#"
-INSERT INTO streamhooks_auth ( access_token )
-VALUES ( ?1 )
-        "#,
-        request.access_token
-    )
-    .execute(&mut *conn)
-    .await?
-    .last_insert_rowid();
-
-    println!("Inserted at {id}");
-
-    let queries = sqlx::query!(
-        r#"
-SELECT access_token
-FROM streamhooks_auth
-        "#
-    )
-    .fetch_all(&pool)
-    .await?;
-
-    for query in queries {
-        println!("Key: {}", query.access_token.unwrap());
+    if validate_auth_token(&pool).await.unwrap() != true {
+        let request = spawn_blocking(move || authenticate_streamhooks().unwrap())
+            .await
+            .unwrap();
+        store_auth_token(&pool, request.access_token).await;
     }
 
     Ok(())
 }
 
-fn validate_auth_token() {
-    todo!()
+async fn validate_auth_token(pool: &sqlx::SqlitePool) -> Result<bool, Error> {
+    let mut auth_token = retrieve_auth_token(pool).await;
+    match auth_token {
+        None => return Ok(false),
+        Some(token) => auth_token = Some(token),
+    };
+    
+    let header_auth_string = format!("OAuth {}", auth_token.unwrap()).parse().unwrap();
+
+    println!("Validating Auth Token");
+
+    let mut headers = header::HeaderMap::new();
+    headers.insert(
+        "Authorization",
+        header_auth_string,
+    );
+
+    let client = reqwest::Client::builder()
+        .redirect(reqwest::redirect::Policy::none())
+        .build()
+        .unwrap();
+
+    let res = client.get("https://id.twitch.tv/oauth2/validate")
+        .headers(headers)
+        .send()
+        .await?
+        .status();
+    
+    if res.is_success() == true {
+        println!("Auth Token is valid!");
+        return Ok(true)
+    } else {
+        println!("Auth Token is invalid, generating new token.");
+        return Ok(false)
+    }
+}
+
+async fn retrieve_auth_token(pool: &sqlx::SqlitePool) -> Option<String> {
+    let query = sqlx::query!(
+        r#"
+SELECT access_token
+FROM streamhooks_auth
+LIMIT 1
+        "#
+    )
+    .fetch_optional(pool)
+    .await
+    .unwrap();
+
+    match query {
+        None => return None,
+        Some(token) => return token.access_token,
+    };
+}
+
+async fn store_auth_token(pool: &sqlx::SqlitePool, auth_token: String) {
+    let mut conn = pool.acquire().await.unwrap();
+
+    println!("Clearing previous key..");
+    sqlx::query!(
+        r#"
+DELETE FROM streamhooks_auth
+        "#
+    )
+    .execute(&mut *conn)
+    .await
+    .unwrap();
+    println!("Previous key cleared!");
+
+    println!("Inserting into database!");
+    sqlx::query!(
+        r#"
+INSERT INTO streamhooks_auth ( access_token )
+VALUES ( ?1 )
+        "#,
+        auth_token
+    )
+    .execute(&mut *conn)
+    .await
+    .unwrap()
+    .last_insert_rowid();
+    println!("Succesfully inserted!");
 }
 
 fn authenticate_streamhooks() -> Result<OauthAccessToken, Error> {
